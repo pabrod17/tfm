572d117ae72d2e61f9d14162fb40e9bb6c0279c5
pablo <pabrod17@gmail.com>
2024-03-09 05:53:22 +0100

    Create filter by dates in trainingHome section and fix error with redux and react versions in the package-box

--- frontend/src/modules/trainings/components/TrainingHome.js
+++ frontend/src/modules/trainings/components/TrainingHome.js
@@ -1,15 +1,24 @@
-import React, {useState} from 'react';
-import {useSelector} from 'react-redux';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import * as actions from '../actions';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import * as selectors from '../selectors';
 import Trainings from './Trainings';
 import * as selectorsTeams from '../../teams/selectors';
 import * as selectorsSeasons from '../../seasons/selectors';
-import {Errors} from '../../common';
-import {FormattedMessage} from 'react-intl';
+import { Errors } from '../../common';
+import { FormattedMessage } from 'react-intl';
+
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import { DateRangePicker } from "react-dates";
+import DatePresets from "./DatePresets";
+import { format } from 'date-fns'; // Importa la función format de date-fns
+import Box from '@mui/material/Box';
+import { Button, IconButton, Pagination, Stack, Toolbar } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 const TrainingHome = () => {
 
@@ -23,106 +32,161 @@ const TrainingHome = () => {
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
     const [backendErrors, setBackendErrors] = useState(null);
+
+    const dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"; // Formato ISO 8601
+    const [dateInput, setDateInput] = useState(null);
+
     let form;
 
-
+    const formattedStartDate = startDate ? startDate.toDate().toISOString() : null;
+    const formattedEndDate = endDate ? endDate.toDate().toISOString() : null;
 
     const handleSubmit = event => {
         event.preventDefault();
-        if (form.checkValidity()) {
 
-            if(team == null) {
-                if(season == null){
-                    dispatch(actions.findTrainingsByTwoDates(
-                        startDate, endDate,
-                        errors => setBackendErrors(errors),
-                    ));
-                } else{
-                    dispatch(actions.findTrainingsByTwoDatesAndSeasondId(
-                        season.id, startDate, endDate,
-                        errors => setBackendErrors(errors),
-                    ));
-                }
-            } else{
-                if(season == null){
-                    dispatch(actions.findTrainingsByTwoDatesAndTeamId(
-                        team.id,startDate, endDate,
-                        errors => setBackendErrors(errors),
-                    ));
-                } else{
-                    dispatch(actions.findTrainingsByTwoDatesAndTeamIdOrSeasonId(
-                        team.id, season.id, startDate, endDate,
-                        errors => setBackendErrors(errors),
-                    ));
-                }
+        if (team == null) {
+            if (season == null) {
+                dispatch(actions.findTrainingsByTwoDates(
+                    formattedStartDate, formattedEndDate,
+                    errors => setBackendErrors(errors),
+                ));
+            } else {
+                dispatch(actions.findTrainingsByTwoDatesAndSeasondId(
+                    season.id, formattedStartDate, formattedEndDate,
+                    errors => setBackendErrors(errors),
+                ));
             }
-
-
-
-            // dispatch(actions.findTrainingsByTwoDatesAndTeamIdOrSeasonId(
-            //     null, null, startDate, endDate,
-            //     errors => setBackendErrors(errors),
-            // ));
-
         } else {
-            setBackendErrors(null);
-            form.classList.add('was-validated');
+            if (season == null) {
+                dispatch(actions.findTrainingsByTwoDatesAndTeamId(
+                    team.id, formattedStartDate, formattedEndDate,
+                    errors => setBackendErrors(errors),
+                ));
+            } else {
+                dispatch(actions.findTrainingsByTwoDatesAndTeamIdOrSeasonId(
+                    team.id, season.id, formattedStartDate, formattedEndDate,
+                    errors => setBackendErrors(errors),
+                ));
+            }
         }
+
+
+
+        // dispatch(actions.findTrainingsByTwoDatesAndTeamIdOrSeasonId(
+        //     null, null, startDate, endDate,
+        //     errors => setBackendErrors(errors),
+        // ));
+
     }
 
-    return(
-        <div>
-            <div>
-                <div className="btn-group white-space mx-auto">
-                    <div class="btn-group mr-5 mb-5 " role="group" aria-label="First group">
-                        <button className="btn addplayer" onClick={() => history(`/trainings/addTraining`)}><FormattedMessage id="project.trainings.fields.addTraining"/></button>
-                    </div>
-                </div>
+    const handleSetAllTrainings = (dispatch) => {
+        dispatch(actions.findTrainingsByUserId(() => history('/trainings/home')));
+    }
 
-                <div>
-                    <Errors errors={backendErrors} onClose={() => setBackendErrors(null)}/>
-                    <div className="">
-                        <div>
-                            <form ref={node => form = node}
-                                className="needs-validation" noValidate 
-                                onSubmit={e => handleSubmit(e)}>
-                                <div>
-                                    <div className="col-md-6">
-                                        <input type="date" id="startDate" className="form-control"
-                                            value={startDate}
-                                            onChange={e => setStartDate(e.target.value)}
-                                            autoFocus
-                                            required/>
-                                        <div className="invalid-feedback">
-                                            <FormattedMessage id='project.global.validator.required'/>
-                                        </div>
-                                        <input type="date" id="endDate" className="form-control"
-                                            value={endDate}
-                                            onChange={e => setEndDate(e.target.value)}
-                                            autoFocus
-                                            required/>
-                                        <div className="invalid-feedback">
-                                            <FormattedMessage id='project.global.validator.required'/>
-                                        </div>
-                                        <div className="">
-                                            <button type="submit" className="btn btn-primary">
-                                            <FormattedMessage id="project.global.buttons.find"/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+    return (
+        <div className=''>
 
-
-
+            <Box
+                sx={{
+                    maxWidth: { xs: 320, sm: 480 },
+                    bgcolor: 'background.dark',
+                    boxShadow: 1,
+                    borderRadius: 4,
+                    margin: 'auto',  // Centra horizontalmente
+                    marginTop: '50px', // Ajusta la distancia desde la parte superior según sea necesario
+                    textAlign: 'center', // Centra el contenido dentro del Box
+                }}>
+<div>
+                <Errors errors={backendErrors} onClose={() => setBackendErrors(null)} />
+                <DateRangePicker
+                    startDate={startDate}
+                    startDateId="s_id"
+                    endDate={endDate}
+                    endDateId="e_id"
+                    onDatesChange={({ startDate, endDate }) => {
+                        console.log("ayuuiuiiii: ", startDate);
+                        setStartDate(startDate);
+                        setEndDate(endDate);
+                    }}
+                    focusedInput={dateInput}
+                    onFocusChange={(e) => setDateInput(e)}
+                    dateFormat={dateFormat}
+                    renderCalendarInfo={() => (
+                        <DatePresets
+                            startDate={startDate}
+                            endDate={endDate}
+                            dateFormat={dateFormat}
+                            handlePresets={(start, end) => {
+                                setStartDate(start);
+                                setEndDate(end);
+                            }}
+                        />
+                    )}
+                />
             </div>
-            <div>
-                <Trainings trainings={trainings.trainings}/>
+            <div className="mt-3">
+                <button type="button" className="button_all_buscar" onClick={handleSubmit}>
+                <FormattedMessage id="project.global.buttons.find" />
+                </button>
+                <button type="button" className="button_all" onClick={() => handleSetAllTrainings(dispatch)}>
+                    All
+                </button>
             </div>
+
+            </Box>
+            <Box
+                sx={{
+                    maxWidth: { xs: 320, sm: 480 },
+                    margin: 'auto',  // Centra horizontalmente
+                    textAlign: 'center', // Centra el contenido dentro del Box
+                }}>
+                <IconButton >
+                    <AddCircleOutlineIcon sx={{
+                        margin: 'auto',  // Centra horizontalmente
+                        textAlign: 'center', // Centra el contenido dentro del Box
+                        fontSize: "70px",
+                        bgcolor: "linear-gradient(147deg,#ffffff ,#4400f9,#000000 35% 70%,#660bd8,#ffffff)",
+                        color: "white"
+                    }}
+                        onClick={() => history(`/stretchings/addStretching`)}
+                    >
+                    </AddCircleOutlineIcon>
+                </IconButton>
+            </Box>
+            <Trainings trainings={trainings.trainings} />
+
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     );
 }
 
