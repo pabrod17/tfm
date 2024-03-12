527e0e0c53eca16fd5458f0063ebf4aaf8f41c24
pablo <pabrod17@gmail.com>
2024-03-12 03:42:24 +0100

    Add modal to show lesion description and medication

--- frontend/src/modules/lesion/components/Lesions.js
+++ frontend/src/modules/lesion/components/Lesions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, createContext } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
@@ -18,6 +18,8 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { useTheme } from '@mui/material/styles';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 const handleRemoveLesion = (id, dispatch, history) => {
   dispatch(actions.removeLesion(id, () => history(`/lesion/home`)));
@@ -32,69 +34,138 @@ const handleViewLesion = (id, dispatch, history) => {
   dispatch(actions.findLesionById(id, () => history(`/lesion/view/${id}`)));
 }
 
-function LesionsList({ items, fallback, dispatch, history, open, setOpen, handleClickOpen, handleClose }) {
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 900,
+  bgcolor: 'rgba(0, 89, 255, 0.25)',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+
+const LesionCard = ({ dispatch, history, item, handleOpenDescriptionModal, handleOpenMedicationModal }) => {
+  return (
+    <div key={item.id}>
+      <div>
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <div className="card">
+                <img src={lesionPierna} alt="Person" className="card__image_lesion"></img>
+                <span className="title">{item.lesionName}</span>
+                <div className="buttons">
+                  <button className="post">{item.lesionType}</button>
+                </div>
+              </div>
+            </div>
+            <div className="flip-card-back">
+              <div className="card">
+                <a onClick={() => handleOpenDescriptionModal(item.description)} class="scroll_efect">
+                  <span className="desc">{item.description}</span>
+                </a>
+                <hr></hr>
+                <a onClick={() => handleOpenMedicationModal(item.medication)} class="button_apple scroll_efect_objective" >
+                  <span class="desc">{item.medication}</span>
+                </a>
+                <hr></hr>
+              </div>
+              <ul class="social-icons trashgrande trash_position">
+                <li><a type="button" onClick={() => handleRemoveLesion(item.id, dispatch, history)}>
+                  <i class="fa fa-trash"></i></a></li>
+              </ul>
+              <ul class="social-icons configgrande config_position">
+                <li><a type="button" onClick={() => handleUpdateLesion(item.id, dispatch, history)}>
+                  <i class="fa fa-wrench"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function LesionsList({ items, fallback, dispatch, history, openDescription, openMedication, handleOpenDescription, handleOpenMedication, handleClose }) {
   if (!items || items.length === 0) {
     dispatch(actions.findAllLesion(() => history('/lesion/home')));
     return fallback;
   } else {
-    return items.map(item => {
-      return <div key={item.id}>
-        <div>
-          <div class="flip-card">
-            <div class="flip-card-inner">
-              <div class="flip-card-front">
-                <div class="card">
-                  <img src={lesionPierna} alt="Person" class="card__image_lesion"></img>
-                  <span class="title">{item.lesionName}</span>
-                  <div class="buttons">
-                    <button class="post">{item.lesionType}</button>
-                  </div>
-                </div>
-              </div>
-              <div class="flip-card-back">
-                <div class="card">
-                  <span class="desc">{item.description}</span>
-                  <a href="#" class="button ">
-                    <span class="desc ">{item.medication}</span>
-                  </a>
-                </div>
-                <ul class="social-icons trashgrande trash_position">
-                <li><a type="button" onClick={() => handleRemoveLesion(item.id, dispatch, history)}>
-                  <i class="fa fa-trash"></i></a></li>
-                </ul>
-                <ul class="social-icons configgrande config_position">
-                    <li><a type="button" onClick={() => handleUpdateLesion(item.id, dispatch, history)}>
-                    <i class="fa fa-wrench"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>;
-    });
+    return items.map(item => (
+      <LesionCard dispatch={dispatch} history={history} key={item.id} item={item} handleOpenDescriptionModal={handleOpenDescription} handleOpenMedicationModal={handleOpenMedication} />
+    ));
   }
 }
 
 const Lesions = ({ lesions }) => {
   const dispatch = useDispatch();
   const history = useNavigate();
-  const [open, setOpen] = React.useState(false);
-  const theme = useTheme();
+  const [modalDescription, setModalDescription] = useState('');
+  const [modalMedication, setModalMedication] = useState('');
+  const [openDescription, setOpenDescription] = React.useState(false);
+  const [openMedication, setOpenMedication] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const handleOpenDescription = (description) => {
+    setModalDescription(description);
+    setOpenDescription(true);
+  };
+
+  const handleOpenMedication = (medication) => {
+    setModalMedication(medication);
+    setOpenMedication(true);
   };
 
   const handleClose = () => {
-    setOpen(false);
+    setModalDescription('');
+    setModalMedication('');
+    setOpenDescription(false);
+    setOpenMedication(false);
   };
+
   return (
 
     <div className="card-group lesions_contaner">
 
       <LesionsList items={lesions} fallback={"Loading..."} dispatch={dispatch} history={history}
-        open={open} setOpen={setOpen} handleClickOpen={handleClickOpen} handleClose={handleClose}
+        openDescription={openDescription} openMedication={openMedication} handleOpenDescription={handleOpenDescription} handleOpenMedication={handleOpenMedication}
       />
+      {(openDescription || openMedication) && (
+        <div className="modal-backdrop" onClick={handleClose}></div>
+      )}
+      {openDescription && (
+        <Modal
+          open={openDescription}
+          onClose={handleClose}
+          aria-labelledby="child-modal-title"
+          aria-describedby="child-modal-description"
+        >
+          <Box sx={{ ...style, width: "auto" }}>
+            <h2 id="child-modal-title" className="color_modal_title_lesion" sx={{ mb: '100px' }} ><FormattedMessage id="project.exercises.fields.description" />:</h2>
+            <p id="child-modal-description">
+              {modalDescription}
+            </p>
+          </Box>
+        </Modal>
+      )}
+      {openMedication && (
+        <Modal
+          open={openMedication}
+          onClose={handleClose}
+          aria-labelledby="child-modal-title"
+          aria-describedby="child-modal-objective"
+        >
+          <Box sx={{ ...style, width: "auto" }}>
+            <h2 id="child-modal-title" className="color_modal_title_lesion" sx={{ mb: '100px' }} ><FormattedMessage id="project.lesion.fields.medication" />:</h2>
+            <p id="child-modal-objective">
+              {modalMedication}
+            </p>
+          </Box>
+        </Modal>
+      )}
     </div>
   )
 };
