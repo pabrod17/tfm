0028798bbe5e17d084a2590c0582b05cb69b274d
pablo <pabrod17@gmail.com>
2024-04-05 22:40:15 +0200

    Create functionality to download board court

--- frontend/src/modules/plays/components/BoardHome.js
+++ frontend/src/modules/plays/components/BoardHome.js
@@ -40,6 +40,7 @@ import Typography from '@mui/material/Typography';
 import drawCourt from './drawCourt.png';
 import drawCourt2 from './courtDraw2.png';
 
+
 const BoardHome = () => {
     const { id } = useParams();
     const plays = useSelector(selectors.getPlays);
@@ -50,6 +51,134 @@ const BoardHome = () => {
     const [brush, setThick] = useState(50);
     const canvasDrawRef = useRef(null);
     const secondCanvasRef = useRef(null);
+
+
+
+    const combineDrawing = (canvasRef) => {
+        
+        const background = canvasRef.current.canvasContainer.children[0]; 
+        const width = background.width;
+        const height = background.height;
+        console.log("ancho: ", width)
+        console.log("alto: ", height)
+        // const width = canvasRef.current.props.canvasWidth;
+        // const height = canvasRef.current.props.canvasHeight;
+        const drawing = canvasRef.current.canvasContainer.children[1]; 
+        const canvas = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+
+        
+
+    
+        // composite now
+        canvas.getContext('2d').drawImage(background, 0, 0);
+        canvas.getContext('2d').globalAlpha = 1.0; 
+        canvas.getContext('2d').drawImage(drawing, 0, 0);
+    
+        const dataUri = canvas.toDataURL('image/jpeg', 1.0);
+        const data = dataUri.split(',')[1];
+        const mimeType = dataUri.split(';')[0].slice(5);
+    
+        const bytes = window.atob(data);
+        const buf = new ArrayBuffer(bytes.length);
+        const arr = new Uint8Array(buf);
+    
+        for (let i = 0; i < bytes.length; i++) {
+            arr[i] = bytes.charCodeAt(i);
+        }
+    
+        const blob = new Blob([arr], { type: mimeType });
+        return { blob: blob, dataUri: dataUri };
+    }
+
+    const saveImage = (blob, filename) => {
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.style = 'display: none';
+    
+        const url = window.URL.createObjectURL(combineDrawing(canvasDrawRef).blob);
+        a.href = url;
+        a.download = "hola.png";
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+
+    // const saveImage4 = () => {
+      
+    //       const image = canvasDrawRef.current.canvasContainer.children[0].toDataURL();
+    //       console.log(canvasDrawRef.current.canvasContainer.children)
+    //       console.log(canvasDrawRef.current.canvasContainer.children[0].length);
+    //       const drawing = canvasDrawRef.current.canvasContainer.children[6].toDataURL();
+
+    //       const link = document.createElement('a');
+    //       link.href = drawing;
+    //       link.download = 'canvas.png';
+    //       link.click();
+    //   };
+
+    //   const saveImage3 = () => {
+    //     const canvas = document.createElement('canvas');
+    //     const context = canvas.getContext('2d');
+        
+    //     // Obtener el tamaño del lienzo
+    //     const width = 1600;
+    //     const height = 1000;
+        
+    //     // Establecer el tamaño del lienzo
+    //     canvas.width = width;
+    //     canvas.height = height;
+        
+    //     // Dibujar el fondo en el lienzo
+    //     const backgroundImage = new Image();
+    //     backgroundImage.src = canvasDrawRef.current.canvasContainer.children[0].toDataURL(); // Fondo
+    //     context.drawImage(backgroundImage, 1, 1, width, height);
+    //     console.log(backgroundImage);
+    //     // Dibujar el dibujo en el lienzo
+    //     const drawingImage = new Image();
+    //     drawingImage.src = canvasDrawRef.current.canvasContainer.children[1].toDataURL(); // Dibujo
+    //     context.drawImage(drawingImage, 0, 0, width, height);
+    //     console.log(context);
+
+    //     // Obtener la URL de los datos del lienzo combinado
+    //     const combinedDataURL = context.canvas.toDataURL('image/png');
+        
+    //     // Crear un enlace para descargar la imagen combinada
+    //     const link = document.createElement('a');
+    //     link.href = combinedDataURL;
+    //     link.download = 'combined_canvas.png';
+    //     link.click();
+    // };
+
+
+
+    //   const saveImage2 = () => {
+    //     /* const uri = refDraw[0].current.toDataURL();
+    //     console.log(uri); */
+    //     const data = canvasDrawRef.current.getSaveData();
+    //     console.log(data);
+    
+    //     const image = canvasDrawRef.current.canvasContainer.childNodes[1].toDataURL();
+    
+    //     const testIMG = canvasDrawRef.current.canvasContainer.children[1].toDataURL();
+    
+    //     console.log(testIMG);
+    
+    //     const d = canvasDrawRef.current.canvasContainer.children[1].toDataURL();
+    //     const w = window.open("about:blank", "image from canvas");
+    //     const img = require("./courtDraw2.png");
+    //     w.document.write(
+    //       "<img src='" +
+    //         d +
+    //         "' style='background-image: url( " +
+    //         img +
+    //         "); background-size: contain;background-repeat: no-repeat;  ' alt='Exporting'/>"
+    //     );
+    //     /* const uri = data.toDataURL();
+    //     console.log(uri); */
+    //   };
+
+
 
     // const handleClick = () =>{
     //     const data = canvasDrawRef.current.getSaveData();
@@ -125,6 +254,8 @@ const BoardHome = () => {
                     >
                         CLEAR
                     </button>
+
+                    <button onClick={saveImage}>Save Image</button>
                     <input
                         min="2"
                         max="50"
