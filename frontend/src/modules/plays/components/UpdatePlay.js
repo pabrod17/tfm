d5079342a8e3b02071aef03d10dd30269729c87a
pablo <pabrod17@gmail.com>
2024-04-28 07:26:13 +0200

    Update play done

--- frontend/src/modules/plays/components/UpdatePlay.js
+++ frontend/src/modules/plays/components/UpdatePlay.js
@@ -91,22 +91,23 @@ const UpdatePlay = () => {
 			my={4}
 			display="flex"
 			alignItems="center"
-			gap={4}
-			p={5}
-			m={10}
+			p={3.35}
+			gap={1}
+			m={6.7}
+			mb={0}
 			sx={{
-                maxWidth: { sm: 1635 },
-				border: '2px solid grey',
+                maxWidth: { sm: 1095 },
+				border: '1.34px solid grey',
                 background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-				borderRadius: "20px",
+				borderRadius: "13.4px",
 				flexWrap: 'wrap',  // Permite que los elementos se envuelvan cuando no hay suficiente ancho
 				flexDirection: 'column',  // Coloca los elementos en una columna cuando el ancho es insuficiente
 				borderColor:"black",
-				boxShadow:"0 10px 50px rgb(0, 0, 0)"
+				boxShadow:"0 6.7px 33.5px rgb(0, 0, 0)"
 			}}
 		>
             <Errors errors={backendErrors} onClose={() => setBackendErrors(null)} />
-			<Grid container margin={5} spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}
+			<Grid container margin={3.35} spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}
 			>
 				<Grid item md={12}>
                 <img src={notaLapiz} alt="Person" class="card__image_play_update_create"></img>
@@ -119,9 +120,11 @@ const UpdatePlay = () => {
 					<Box
 						component="form"
 						sx={{
-							borderRadius: "20px",
+							borderRadius: "13.4px",
 							borderColor:"black",
-                            boxShadow:"0 10px 50px rgb(0, 0, 0)"
+                            boxShadow:"0 6.7px 33.5px rgb(0, 0, 0)",
+							marginBottom:"0px",
+							marginTop:"-21px"
 						}}
 						autoHeight={true} // Permitir que la tabla determine su propio tamaño si los datos no se han cargado
 						noValidate
@@ -133,9 +136,9 @@ const UpdatePlay = () => {
                                 
                                 component="form"
                                 sx={{
-                                    '& .MuiTextField-root': { mb: 2, width: '100%' },
-                                    margin: '50px', // Centra el formulario en la pantalla
-                                    mb:"-50px",
+									'& .MuiTextField-root': { mb: 1.34, width: '100%' },
+									margin: '33.5px', // Centra el formulario en la pantalla
+                                    mb:"-70px",
                                     mt:"-3px"
                                 }}
                                 noValidate
@@ -145,8 +148,7 @@ const UpdatePlay = () => {
 	  <InputLabel id="demo-simple-select-label"
               sx={{
                 color: "#00bfff",
-				fontSize:"20px",
-                marginLeft:"10px"
+				fontSize:"13.40px",
               }}
 
             ><FormattedMessage id="project.lesion.fields.lesionType" /></InputLabel>
@@ -158,12 +160,12 @@ const UpdatePlay = () => {
           onChange={(e) => setPlayType(e.target.value)}
 		  sx={{
 			color: "white",
-			border: '2px solid grey',
+			border: '1.34px solid grey',
 			background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-			borderRadius: "20px",
+			borderRadius: "13.4px",
 			borderColor:"black",
 			boxShadow:"0 10px 10px rgb(0, 0, 0)",
-            marginLeft:"10px"
+			height:"40px",
 
 		  }}
 		  inputProps={{
@@ -191,9 +193,10 @@ const UpdatePlay = () => {
                                 
 									component="form"
 									sx={{
-										'& .MuiTextField-root': { mb: 2, width: '100%' },
-										margin: '50px', // Centra el formulario en la pantalla
-
+										'& .MuiTextField-root': { mb: 1.34, width: '100%' },
+										margin: '33.5px', // Centra el formulario en la pantalla
+										marginTop:"50px",
+										marginBottom:"10px"
 									}}
 									noValidate
 									autoComplete="off"
@@ -201,16 +204,16 @@ const UpdatePlay = () => {
 									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.plays.fields.pointGuardText" />}
-										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={pointGuardText}
 										onChange={(e) => setPointGuardText(e.target.value)}
@@ -218,16 +221,16 @@ const UpdatePlay = () => {
 									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.plays.fields.shootingGuardText" />}
-										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={shootingGuardText}
 										onChange={(e) => setShootingGuardText(e.target.value)}
@@ -235,16 +238,16 @@ const UpdatePlay = () => {
 									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.plays.fields.smallForwardText" />}
-										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={smallForwardText}
 										onChange={(e) => setSmallForwardText(e.target.value)}
@@ -252,16 +255,16 @@ const UpdatePlay = () => {
 									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.plays.fields.powerForwardText" />}
-										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={powerForwardText}
 										onChange={(e) => setPowerForwardText(e.target.value)}
@@ -275,8 +278,10 @@ const UpdatePlay = () => {
                                 
 									component="form"
 									sx={{
-										'& .MuiTextField-root': { mb: 2, width: '100%' },
-										margin: '50px', // Centra el formulario en la pantalla
+										'& .MuiTextField-root': { mb: 1.34, width: '100%' },
+										margin: '33.5px', // Centra el formulario en la pantalla
+										marginTop:"50px",
+										marginBottom:"10px"
 
 									}}
 									noValidate
@@ -285,16 +290,16 @@ const UpdatePlay = () => {
 									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.plays.fields.centerText" />}
-										InputLabelProps={{ style: { color: '#E8FF00', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#00bfff', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={centerText}
 										onChange={(e) => setCenterText(e.target.value)}
@@ -302,16 +307,16 @@ const UpdatePlay = () => {
 									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.notes.fields.title" />}
-										InputLabelProps={{ style: { color: '#00bfff', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#00bfff', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={title}
 										onChange={(e) => setTitle(e.target.value)}
@@ -319,16 +324,16 @@ const UpdatePlay = () => {
                                     									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.plays.fields.gesture" />}
-										InputLabelProps={{ style: { color: '#00bfff', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#00bfff', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={gesture}
 										onChange={(e) => setGesture(e.target.value)}
@@ -336,16 +341,16 @@ const UpdatePlay = () => {
                                     									<TextField
 										id="outlined-multiline-static-1"
 										label={<FormattedMessage id="project.exercises.fields.description" />}
-										InputLabelProps={{ style: { color: '#00bfff', fontSize: 20, fontWeight: 'regular', width: '100%' } }}
-										InputProps={{ style: { color: 'white', padding: '10px', fontSize: 15, fontWeight: 'regular', width: '100%' } }}
+										InputLabelProps={{ style: { color: '#00bfff', fontSize: 13.40, fontWeight: 'regular', width: '100%', top:-5 } }}
+										InputProps={{ style: { color: 'white', padding: '6.7px', fontSize: 12, fontWeight: 'regular', width: '100%' } }}
 										multiline
 										rows={4}
 										sx={{
-											border: '2px solid grey',
+											border: '1.34px solid grey',
+											borderRadius: "13.4px",
 											background: "linear-gradient(-45deg, #41AF24 0%, #062C76 50% )",
-											borderRadius: "20px",
 											borderColor:"black",
-											boxShadow:"0 10px 10px rgb(0, 0, 0)"
+											boxShadow:"0 6.7px 6.7px rgb(0, 0, 0)"
 										}}
 										value={description}
 										onChange={(e) => setDescription(e.target.value)}
