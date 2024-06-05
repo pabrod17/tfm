4301f4cd12d09cd516a080640997f2a9171c06aa
pablo <pabrod17@gmail.com>
2024-06-06 01:22:56 +0200

    Club categories design done

--- tfmMobile/app/src/main/java/com/example/tfmmobile/ui/club/adapter/categories/CategoriesViewHolder.kt
+++ tfmMobile/app/src/main/java/com/example/tfmmobile/ui/club/adapter/categories/CategoriesViewHolder.kt
@@ -1,13 +1,17 @@
 package com.example.tfmmobile.ui.club.adapter.categories
 
+import android.graphics.Color
+import android.os.Build
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getString
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tfmmobile.R
 import com.example.tfmmobile.ui.club.ClubCategory
+import com.google.android.material.card.MaterialCardView
 
 class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
@@ -16,7 +20,11 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     //    private val divider:View = view.findViewById(R.id.divider)
     private val cardClubLayout: View = view.findViewById(R.id.cardClubLayout)
     private val cardImage: AppCompatImageView = view.findViewById(R.id.cardImage)
+    private val clubCategoryPrincipal: MaterialCardView = view.findViewById(R.id.clubCategoryPrincipal)
+    private val ivTeamDetail: MaterialCardView = view.findViewById(R.id.ivTeamDetail)
 
+
+    @RequiresApi(Build.VERSION_CODES.P)
     fun render(clubCategory: ClubCategory, isSelected: Boolean, onItemSelected: (Int) -> Unit) {
         tvCategoryName.text = "Ejemplo"
 
@@ -33,11 +41,28 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                     R.drawable.gradient_background_club_category_player
                 )
                 cardImage.setImageResource(R.drawable.player)
+                clubCategoryPrincipal.strokeWidth = 0
+                clubCategoryPrincipal.cardElevation = 10F
+                clubCategoryPrincipal.outlineSpotShadowColor = Color.TRANSPARENT
+
+                ivTeamDetail.strokeWidth = 0
+                ivTeamDetail.cardElevation = 10F
+                ivTeamDetail.outlineSpotShadowColor = Color.TRANSPARENT
+
                 if (clubCategory.isSelected) {
-                    cardClubLayout.background = ContextCompat.getDrawable(
-                        cardClubLayout.context,
-                        R.drawable.gradient_background_club_category_player_selected
-                    )
+//                    cardClubLayout.background = ContextCompat.getDrawable(
+//                        cardClubLayout.context,
+//                        R.drawable.gradient_background_club_category_player_selected
+//                    )
+                    clubCategoryPrincipal.strokeWidth = 4
+                    clubCategoryPrincipal.strokeColor = Color.WHITE
+                    clubCategoryPrincipal.outlineSpotShadowColor = Color.WHITE
+                    clubCategoryPrincipal.cardElevation = 40F
+
+                    ivTeamDetail.strokeWidth = 4
+                    ivTeamDetail.strokeColor = Color.WHITE
+                    ivTeamDetail.outlineSpotShadowColor = Color.WHITE
+                    ivTeamDetail.cardElevation = 40F
                 }
 
 
@@ -51,11 +76,28 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                     R.drawable.gradient_background_club_category_season
                 )
                 cardImage.setImageResource(R.drawable.season)
+                clubCategoryPrincipal.strokeWidth = 0
+                clubCategoryPrincipal.cardElevation = 10F
+                clubCategoryPrincipal.outlineSpotShadowColor = Color.TRANSPARENT
+
+                ivTeamDetail.strokeWidth = 0
+                ivTeamDetail.cardElevation = 10F
+                ivTeamDetail.outlineSpotShadowColor = Color.TRANSPARENT
+
                 if (clubCategory.isSelected) {
-                    cardClubLayout.background = ContextCompat.getDrawable(
-                        cardClubLayout.context,
-                        R.drawable.gradient_background_club_category_season_selected
-                    )
+//                    cardClubLayout.background = ContextCompat.getDrawable(
+//                        cardClubLayout.context,
+//                        R.drawable.gradient_background_club_category_season_selected
+//                    )
+                    clubCategoryPrincipal.strokeWidth = 4
+                    clubCategoryPrincipal.strokeColor = Color.WHITE
+                    clubCategoryPrincipal.outlineSpotShadowColor = Color.WHITE
+                    clubCategoryPrincipal.cardElevation = 40F
+
+                    ivTeamDetail.strokeWidth = 4
+                    ivTeamDetail.strokeColor = Color.WHITE
+                    ivTeamDetail.outlineSpotShadowColor = Color.WHITE
+                    ivTeamDetail.cardElevation = 40F
                 }
             }
 
@@ -67,11 +109,28 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                     R.drawable.gradient_background_club_category_team
                 )
                 cardImage.setImageResource(R.drawable.team)
+                clubCategoryPrincipal.strokeWidth = 0
+                clubCategoryPrincipal.cardElevation = 10F
+                clubCategoryPrincipal.outlineSpotShadowColor = Color.TRANSPARENT
+
+                ivTeamDetail.strokeWidth = 0
+                ivTeamDetail.cardElevation = 10F
+                ivTeamDetail.outlineSpotShadowColor = Color.TRANSPARENT
+
                 if (clubCategory.isSelected) {
-                    cardClubLayout.background = ContextCompat.getDrawable(
-                        cardClubLayout.context,
-                        R.drawable.gradient_background_club_category_team_selected
-                    )
+//                    cardClubLayout.background = ContextCompat.getDrawable(
+//                        cardClubLayout.context,
+//                        R.drawable.gradient_background_club_category_team_selected
+//                    )
+                    clubCategoryPrincipal.strokeWidth = 4
+                    clubCategoryPrincipal.strokeColor = Color.WHITE
+                    clubCategoryPrincipal.outlineSpotShadowColor = Color.WHITE
+                    clubCategoryPrincipal.cardElevation = 40F
+
+                    ivTeamDetail.strokeWidth = 4
+                    ivTeamDetail.strokeColor = Color.WHITE
+                    ivTeamDetail.outlineSpotShadowColor = Color.WHITE
+                    ivTeamDetail.cardElevation = 40F
                 }
             }
         }
