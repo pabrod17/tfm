5ace4ed294a387f0dbd38104040ed4049aea3060
pablo <pabrod17@gmail.com>
2024-06-07 01:28:28 +0200

    Plays layout done

--- tfmMobile/app/src/main/java/com/example/tfmmobile/ui/plays/adapter/categories/CategoriesViewHolder.kt
+++ tfmMobile/app/src/main/java/com/example/tfmmobile/ui/plays/adapter/categories/CategoriesViewHolder.kt
@@ -1,19 +1,25 @@
 package com.example.tfmmobile.ui.plays.adapter.categories
 
+import android.graphics.Color
+import android.os.Build
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getString
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tfmmobile.R
 import com.example.tfmmobile.ui.plays.PlaysCategory
+import com.google.android.material.card.MaterialCardView
 
 class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val tvCategoryNamePlays: TextView = view.findViewById(R.id.tvCategoryNamePlays)
 
     private val cardPlayshLayout: View = view.findViewById(R.id.cardPlayshLayout)
+    private val playsCategoryPrincipal: MaterialCardView = view.findViewById(R.id.playsCategoryPrincipal)
 
+    @RequiresApi(Build.VERSION_CODES.P)
     fun render(playsCategory: PlaysCategory, isSelected: Boolean, onItemSelected: (Int) -> Unit) {
         tvCategoryNamePlays.text = "Ejemplo"
 
@@ -28,11 +34,19 @@ class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                     cardPlayshLayout.context,
                     R.drawable.gradient_background_plays_category_play_card
                 )
+                playsCategoryPrincipal.strokeWidth = 0
+                playsCategoryPrincipal.cardElevation = 10F
+                playsCategoryPrincipal.outlineSpotShadowColor = Color.TRANSPARENT
+
                 if (playsCategory.isSelected) {
-                    cardPlayshLayout.background = ContextCompat.getDrawable(
-                        cardPlayshLayout.context,
-                        R.drawable.gradient_background_plays_category_play_card_selected
-                    )
+//                    cardPlayshLayout.background = ContextCompat.getDrawable(
+//                        cardPlayshLayout.context,
+//                        R.drawable.gradient_background_plays_category_play_card_selected
+//                    )
+                    playsCategoryPrincipal.strokeWidth = 4
+                    playsCategoryPrincipal.strokeColor = Color.WHITE
+                    playsCategoryPrincipal.outlineSpotShadowColor = Color.WHITE
+                    playsCategoryPrincipal.cardElevation = 40F
                 }
 
 
